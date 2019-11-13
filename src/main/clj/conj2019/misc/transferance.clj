702ad2befc3f26814b43e0c4c7375df190793534
Mark Bastian <markbastian@gmail.com>
2019-11-12 23:18:56 -0700

    example added

---
+++ src/main/clj/conj2019/misc/transferance.clj
@@ -0,0 +1,19 @@
+(ns conj2019.misc.transferance)
+
+{:eccentricity                    0.8
+ :semimajor-axis                  [19113.0 :km]
+ :inclination                     [42.0 :deg]
+ :longitude-of-the-ascending-node [27.1 :deg]
+ :argument-of-periapsis           [68.9 :deg]
+ :true-anomaly                    [18 :deg]}
+
+[{:name       "Gandalf"
+  :race       :human
+  :class      :magic-user
+  :HP         100
+  :attributes {:STR 13 :CON 13 :DEX 17 :INT 18 :WIS 18 :CHA 16}}
+ {:name       "Frodo"
+  :race       :halfling
+  :class      :thief
+  :HP         24
+  :attributes {:STR 9 :CON 12 :DEX 17 :INT 16 :WIS 12 :CHA 16}}]
